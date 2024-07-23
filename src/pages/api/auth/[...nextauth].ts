6e52d99cd7b666dd3beff07891d12dc0b397e203
Sahil <sahils@internal.digiflux.io>
2024-07-23 17:34:42 +0530

    new commit

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -3,3 +3,21 @@ import NextAuth from "next-auth";
 import { authOptions } from "~/server/auth";
 
 export default NextAuth(authOptions);
+
+// import NextAuth from "next-auth";
+// import GoogleProvider from "next-auth/providers/google";
+// import { PrismaAdapter } from "@next-auth/prisma-adapter";
+// import { PrismaClient } from "@prisma/client";
+
+// const prisma = new PrismaClient();
+
+// export default NextAuth({
+//   providers: [
+//     GoogleProvider({
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     }),
+//   ],
+//   adapter: PrismaAdapter(prisma),
+//   secret: process.env.SECRET,
+// });
